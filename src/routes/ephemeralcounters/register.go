705b39cd52e0f7c439784dbb628cb5949a7af8ae
Nikita Petko <petko@vmminfra.net>
2023-08-16 00:41:51 +0100

    Fix case insensitve routing (pt.4)

--- src/routes/ephemeralcounters/register.go
+++ src/routes/ephemeralcounters/register.go
@@ -29,7 +29,7 @@ func RegisterRoutes(r *mux.Router) {
 			writeAPI = client.WriteAPI(*flags.EphemeralCountersInfluxDbReportingOrganization, *flags.EphemeralCountersInfluxDbReportingDatabase)
 		}
 
-		r.HandleFunc("/{route:v1\\.0\\/[S|s]equence[S|s]tatistics\\/[B|b]atch[A|a]ddto[S|s]equences[V|v]2\\/?}", batchAddToSequencesV2).Methods("POST")
+		r.HandleFunc("/{route:v1\\.0\\/[S|s]equence[S|s]tatistics\\/[B|b]atch[A|a]dd[T|t]o[S|s]equences[V|v]2\\/?}", batchAddToSequencesV2).Methods("POST")
 		r.HandleFunc("/{route:v1\\.1\\/[C|c]ounters\\/[B|b]atch[I|i]ncrement\\/?}", batchIncrement).Methods("POST")
 	})
 }
