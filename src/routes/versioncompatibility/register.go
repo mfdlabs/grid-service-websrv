937a305f1f21aaada96550c57dfbb0717d82dc0b
Nikita Petko <petko@vmminfra.net>
2023-08-16 00:36:54 +0100

    Fix case insensitve routing (pt.3) (#160)

--- src/routes/versioncompatibility/register.go
+++ src/routes/versioncompatibility/register.go
@@ -12,7 +12,7 @@ var registerOnce sync.Once
 // RegisterRoutes registers the version compatibility routes.
 func RegisterRoutes(r *mux.Router) {
 	registerOnce.Do(func() {
-		r.HandleFunc("/{route:getallowedmd5Hashes\\/?}", routes.StubEmpty).Methods("GET")
-		r.HandleFunc("/{route:getallowedversions\\/?}", routes.StubEmpty).Methods("GET")
+		r.HandleFunc("/{route:[G|g]et[A|a]llowed(?:MD|Md|mD|md)5[H|h]ashes\\/?}", routes.StubEmpty).Methods("GET")
+		r.HandleFunc("/{route:[G|g]et[A|a]llowed[V|v]ersions\\/?}", routes.StubEmpty).Methods("GET")
 	})
 }
